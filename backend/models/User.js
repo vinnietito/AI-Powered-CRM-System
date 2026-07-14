893d0c79397b001abd96979e6f196294db430c2b
vinnietito <vincentnjuguna69@gmail.com>
2026-07-14 20:04:55 +0300

    Finished on the User.js model

--- backend/models/User.js
+++ backend/models/User.js
@@ -32,3 +32,16 @@ const userSchema = new mongoose.schema(
     },
     { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+userSchema.methods.matchPassword = function (entered) {
+    return bcrypt.compare(entered, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
