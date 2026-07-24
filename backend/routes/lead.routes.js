f63d718d953dbe59d369be63f5e1470c73394259
vinnietito <vincentnjuguna69@gmail.com>
2026-07-24 20:23:20 +0300

    Created and worked on Lead Routes

---
+++ backend/routes/lead.routes.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+
+import {
+    getLeads,
+    getLead,
+    createLead,
+    updateLead,
+    deleteLead
+} from '../controllers/lead.controller.js';
+import { protect } from '../middleware/auth.middleware.js';
+
+const router = Router();
+
+router.use(protect);
+
+router.patch("/reorder", reorderLeads);
+router.route("/").get(getLeads).post(createLead);
+router.route("/:id").get(getLead).put(updateLead).delete(deleteLead);
+
+export default router;
