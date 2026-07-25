1bcc5fb98d4b1878cd70718153fcd0c27b5303a5
vinnietito <vincentnjuguna69@gmail.com>
2026-07-25 20:38:29 +0300

    Created and wrote the note routes file

---
+++ backend/routes/note.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {
+    getNotes,
+    createNote,
+    updateNote,
+    deleteNote,
+} from "../controllers/note.controller.js";
+import { protect }  from "../middleware/auth.middleware.js";
+
+const router = Router();
+router.use(protect);
+
+router.route("/").get(getNotes).post(createNote);
+router.rooute("/:id").put(updateNote).delete(deleteNote);
+
+export default router;
